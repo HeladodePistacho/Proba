70840fb5a600077de545c6b718332ead47d6699e
HeladodePistacho <th_sola@hotmail.com>
2016-04-07 21:35:06 +0200

    minor changes, better audio performance

--- Outzone/ModuleAudio.cpp
+++ Outzone/ModuleAudio.cpp
@@ -16,13 +16,18 @@ bool ModuleAudio::Init()
 {
 	LOG("Creating audio stuf");
 
-	SDL_Init(SDL_INIT_AUDIO);
+	if (SDL_Init(SDL_INIT_AUDIO) < 0){
+		LOG("error SDLaudio")
+		return false;
+	}
+
 	int flag = MIX_INIT_OGG;
 	int inited_flags = Mix_Init(flag);
-	
-	Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096);
 
+	Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 1024);
 	
+
+
 
 	return true;
 }
@@ -30,13 +35,13 @@ bool ModuleAudio::Init()
 bool ModuleAudio::Start()
 {
 	
+
 	Level_1_Theme = Mix_LoadMUS("1st_Level_Theme.ogg");
 	Mix_PlayMusic(Level_1_Theme, -1);
 	
 	
 	return true;
 }
-
 
 
 
